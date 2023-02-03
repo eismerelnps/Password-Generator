fe9b677e745b69b6a3721e58c92a5517148609da
Eismer Lobaina <113050158+eismerelnps@users.noreply.github.com>
2023-02-02 22:04:36 -0500

    Working till now

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -17,23 +17,31 @@ public class GUI {
     private JRadioButton minCharsRadioButton;
     private JRadioButton numbersCharsRadioButton;
     private JRadioButton specialCharsRadioButton;
-    private JButton button1;
+    private JLabel passwordJLabel;
+    private JTextArea passwordTextArea;
+    private JButton copyButton;
     Generator generator = new Generator();
 
     public GUI() {
         setOptionsPanel.setVisible(false);
+        capitalCharsRadioButton.setSelected(true);
 
         setCharsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (Integer.parseInt(lengthTextField.getText()) <= 4 || Integer.parseInt(lengthTextField.getText()) > 120) {
+                if (lengthTextField.getText().isEmpty()){
                     JOptionPane.showMessageDialog(null,
-                            "For security set e length between 4 and 120 characters ",
+                            "Please set a length first ",
                             "",
                             2,
                             null);
-                }
-                if (!capitalCharsRadioButton.isSelected() && !minCharsRadioButton.isSelected() && !numbersCharsRadioButton.isSelected() && !specialCharsRadioButton.isSelected()) {
+                } else if(Integer.parseInt(lengthTextField.getText()) <= 4 || Integer.parseInt(lengthTextField.getText()) > 120) {
+                    JOptionPane.showMessageDialog(null,
+                            "For security set a length between 4 and 120 characters ",
+                            "",
+                            2,
+                            null);
+                } else if (!capitalCharsRadioButton.isSelected() && !minCharsRadioButton.isSelected() && !numbersCharsRadioButton.isSelected() && !specialCharsRadioButton.isSelected()) {
                     JOptionPane.showMessageDialog(null,
                             "Set at least one of the requirements",
                             "",
@@ -41,11 +49,7 @@ public class GUI {
                             null);
                 } else {
                     generator.setOptions(Integer.parseInt(lengthTextField.getText()), capitalCharsRadioButton.isSelected(), minCharsRadioButton.isSelected(), numbersCharsRadioButton.isSelected(), specialCharsRadioButton.isSelected());
-                    JOptionPane.showMessageDialog(null,
-                            generator.generate(Integer.parseInt(lengthTextField.getText())),
-                            "",
-                            2,
-                            null);
+                    passwordJLabel.setText(generator.generate(Integer.parseInt(lengthTextField.getText())));
                 }
             }
         });
